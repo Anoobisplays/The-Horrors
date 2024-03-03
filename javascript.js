b1a22f55f41696325ca7aedca108abc5bfceabd9
Beginner <60657918+Anoobisplays@users.noreply.github.com>
2024-03-02 20:54:41 -0400

    March 2nd Update
    Homepage looking pretty good. Need to fix the alignment of the images

---
+++ javascript.js
@@ -0,0 +1,8 @@
+<script type="text/javascript">
+document.getElementById('searchform').onsubmit = function() {
+    window.location = 'http://www.google.com/search?q=site:yourdomainname ' + document.getElementById('test').value;
+    return false;
+}
+</script>
+
+<link rel="javascript" href="javascript.js"???????
